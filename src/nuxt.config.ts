59ac35bf8f4df7753f9d9d611dd4802288714c7a
appolinarium <appolinarium@proton.me>
2023-05-29 15:29:27 +0000

    Initial {sass, nuxt.config.ts}

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -1,4 +1,32 @@
+import { NoScript } from "./.nuxt/components";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  app: {
+    head: {
+      meta: [
+        {
+          name: 'viewport', content: 'width=device-width, initial-scale=1'
+        }
+      ],
+      noscript: [
+        {
+          children: 'Please, turn up JavaScript or upgrade your browser'
+        }
+      ]
+    },
+    rootId: '__app',
+  },
+  build: {
+    analyze: {
+      analyzerMode: 'static'
+    }
+  },
+  devServer: {
+    port: 8080
+  },
+  extensions: [
+    '.ts',
+    '.vue'
+  ]
 })
