41ec38e58b95a8c5b7ffd28f520aa07f2a6477b9
appolinarium <appolinarium@proton.me>
2023-06-02 17:18:05 +0000

    removing: element plus

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,68 +1,9 @@
 <template>
   <NuxtLayout name="welcome">
-    <el-container class="welcome-container">
-
-
-      <el-main class="welcome-container__content">
-        <HeroHeaderSubheader>
-
-          <template #header>
-            Registration-free user-oriented<br>social network
-          </template>
-
-          <template #subheader>
-            <CounterUsers />users already just remembered the
-          </template>
-
-          <template #action>
-            <FormAuth />
-          </template>
-
-        </HeroHeaderSubheader>
-      </el-main>
-
-
-      <el-footer class="welcome-container__footer">
-        <el-row justify="space-between">
-          <el-col span="8">
-            <TextCopyright
-              name="Identify"
-            />
-          </el-col>
-
-          <el-col span="8">
-            <ListLanguages :items="['Русский', 'Беларуская (тарашкевiца)', 'English']"/>
-          </el-col>
-          
-          <el-col span="8">
-            <el-button link> 
-              <TextLabel>
-                <IconGithub />Source code
-              </TextLabel>
-            </el-button>
-          </el-col>
-          
-        </el-row>
-      </el-footer>
-
-    </el-container>
+    <h1>Entry page<h2>
   </NuxtLayout>
 
 </template>
 
 <style lang="scss">
-  .welcome-container {
-    justify-content: space-between;
-    @include padding(0 300px);
-    @include padding-top(112px);
-    
-    &__content {
-      @include pm-reset();
-    }
-
-    &__footer {
-      @include pm-reset();
-      @include padding(0 100px);
-    }
-  }
 </style>
