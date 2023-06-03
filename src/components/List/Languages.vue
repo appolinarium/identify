f470f765e5caca413c443db6f8ee9e39df8c9ebb
penpen <penpen@mail.com>
2023-06-03 18:29:07 +0000

    initial: Languages. Basic languages panel with no style bindings

--- src/components/List/Languages.vue
+++ src/components/List/Languages.vue
@@ -1,43 +1,50 @@
 <script setup lang="ts">
   const props = defineProps({
-    items: []
+    items: Array
   }) 
-
   const lang = useState('lang', () => 'en')
+
+  function changeLang(e : Object) {
+    lang.value = e.key
+  }
 
 </script>
 
+  
 <template>
-  <ul class="list" >
-    <li v-for="(val, key) in items" @click="lang = key">
-      <span class="list__item list__item--selected" v-if="key === lang">
-        {{ val }}
-      </span>
-      <span class="list__item" v-else>
-        {{ val }}
-      </span>
-    </li>
-  </ul>
+    <div class="card flex justify-content-center">
+      <SelectButton
+      v-on:update:modelValue="changeLang"
+      :options="items"
+      optionLabel="name"/>
+    </div>
 </template>
 
-<style scoped lang="scss">
-  .list {
-    @include ul-reset();
-    display: flex;
-    align-items: center;
-    gap: 20px;
 
-    &__item {
-      font-family: 'Lato';
-      font-weight: normal;
-      font-style: normal;
-      cursor: pointer;
-      color: white;
-      text-transform: capitalize;
-    }
-
-    &__item--selected {
-      color: red;
-    }
+<style lang="scss">
+.p-selectbutton{
+  display: flex;
+  justify-content: space-between;
+}
+.p-selectbutton .p-button {
+  border: none !important;
+  background: none !important;
+  color: #c9cfd9 !important;
+  @include font-size(14px);
+  font-weight: 400;
+  font-family: 'Lato';
+  :hover{
+    color: #fff !important;
   }
+}
+.p-button.p-highlight:hover {
+  border: none !important;
+}
+.p-button:focus{
+  box-shadow: none !important;
+  span{
+    color: $accent;
+  }
+}
+
 </style>
--- src/plugins/primevue.js
+++ src/plugins/primevue.js
@@ -1,9 +1,14 @@
 import { defineNuxtPlugin } from "#app";
 import PrimeVue from "primevue/config";
 import Button from "primevue/button";
+import Dock from "primevue/dock";
+import RadioButton from 'primevue/radiobutton';
+import SelectButton from 'primevue/selectbutton'
 
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.use(PrimeVue, { ripple: true });
     nuxtApp.vueApp.component("Button", Button);
-    //other components that you need
+    nuxtApp.vueApp.component("Dock",Dock)
+    nuxtApp.vueApp.component("RadioButton",RadioButton)
+    nuxtApp.vueApp.component("SelectButton",SelectButton)
 });
