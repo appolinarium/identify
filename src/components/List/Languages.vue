aeeeb022943acd9fdc465914dae39251dc67b76f
appolinarium <appolinarium@proton.me>
2023-06-03 09:42:26 +0000

    initial: Basic languages selector component

--- src/components/List/Languages.vue
+++ src/components/List/Languages.vue
@@ -1,18 +1,43 @@
 <script setup lang="ts">
   const props = defineProps({
     items: []
-  })
+  }) 
+
+  const lang = useState('lang', () => 'en')
+
 </script>
 
 <template>
-  <ListBase class=".list--languages" :items="props.items"/>
+  <ul class="list" >
+    <li v-for="(val, key) in items" @click="lang = key">
+      <span class="list__item list__item--selected" v-if="key === lang">
+        {{ val }}
+      </span>
+      <span class="list__item" v-else>
+        {{ val }}
+      </span>
+    </li>
+  </ul>
 </template>
 
 <style scoped lang="scss">
-  .list--languages {
-    color: var(--el-color-info);
-    gap: 20px;
+  .list {
+    @include ul-reset();
+    display: flex;
     align-items: center;
-    justify-content: space-between;
+    gap: 20px;
+
+    &__item {
+      font-family: 'Lato';
+      font-weight: normal;
+      font-style: normal;
+      cursor: pointer;
+      color: white;
+      text-transform: capitalize;
+    }
+
+    &__item--selected {
+      color: red;
+    }
   }
 </style>
