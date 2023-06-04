85a61230584c6c8d6cc6a7ae479cfb1f806ce1c1
penpen <penpen@mail.com>
2023-06-04 19:24:23 +0000

    initial: {Groups folder,Languages,RadioButton, RadioGroups}

---
+++ src/components/Groups/RadioGroups.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="radio-group"
+        >
+        <slot></slot>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+.radio-group{
+    display: flex;
+    @include rfs(gap,20px);
+    justify-content: center;
+}
+</style>
--- src/components/List/Languages.vue
+++ src/components/List/Languages.vue
@@ -1,50 +1,30 @@
 <script setup lang="ts">
   const props = defineProps({
-    items: Array
-  }) 
-  const lang = useState('lang', () => 'en')
-
-  function changeLang(e : Object) {
-    lang.value = e.key
+    languages: Array
+  })
+  const lang = ref('')
+  const state = useState('lang', () => 'en')
+  watch(lang,(newV) => {changeLang(newV)} )
+  function changeLang(e){
+    state.value = e
   }
 
 </script>
 
-  
 <template>
-    <div class="card flex justify-content-center">
-      <SelectButton
-      v-on:update:modelValue="changeLang"
-      :options="items"
-      optionLabel="name"/>
-    </div>
+  <GroupsRadioGroups>
+    <InputRadioButton   
+      v-for="(item,index) in languages"
+      :label="item.name"
+      :val="item.key"
+      v-model="lang"
+      >
+    </InputRadioButton>
+
+  </GroupsRadioGroups>
 </template>
 
 
 <style lang="scss">
-.p-selectbutton{
-  display: flex;
-  justify-content: space-between;
-}
-.p-selectbutton .p-button {
-  border: none !important;
-  background: none !important;
-  color: #c9cfd9 !important;
-  @include font-size(14px);
-  font-weight: 400;
-  font-family: 'Lato';
-  :hover{
-    color: #fff !important;
-  }
-}
-.p-button.p-highlight:hover {
-  border: none !important;
-}
-.p-button:focus{
-  box-shadow: none !important;
-  span{
-    color: $accent;
-  }
-}
 
 </style>
