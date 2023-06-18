a31bf911fb4404f851c85591acf965dfe1572d66
appolinarium <appolinarium@proton.me>
2023-06-18 13:54:29 +0000

    initial: English locale

---
+++ src/localization/en.js
@@ -0,0 +1,22 @@
+{
+  app: {
+    description: 'Registration-free user-oriented social network'
+  },
+  entry: {
+    subtitle: 'users already just remembered the {accent}',
+    accent: 'private key'
+  },
+  input: {
+    key: {
+      tip: 'Input your private key'
+    }
+  },
+  checkbox: {
+    license: 'I confirm the license agreement',
+    session: 'Save session in browser'
+  },
+  sourceCode: "Source code",
+  tip: {
+    privateKey: 'Private keys are personal user identifiers used for authorization. They are based on symmetric encryption combined with optimizing Identify systems. Read more in our development guide.'
+  }
+}
